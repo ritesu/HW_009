f6a76e1dd72d47bcf7525415a043aeecae054f08
ritesu <ritesu@mail.ru>
2023-02-14 03:01:22 +1100

    Task_009

--- Task_009/Program.cs
+++ Task_009/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите первых два числа последовательности");
-int a = int.Parse(Console.ReadLine()??"");
-int b = int.Parse(Console.ReadLine()??"");
+int a = int.Parse(Console.ReadLine() ?? "");
+int b = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Введите количество чисел, которое нужно отобразить");
-int numb = int.Parse(Console.ReadLine()??"");
+int numb = int.Parse(Console.ReadLine() ?? "");
 int[] numbers = new int[numb];
 void MathNumbers()
 {
@@ -11,23 +11,23 @@ void MathNumbers()
     int temp = b - a;
     for (int i = 2; i < numbers.Length; i++)
     {
-        numbers[i] = numbers[i-1] + temp;
+        numbers[i] = numbers[i - 1] + temp;
     }
 }
 
 void PrintResult()
 {
     for (int i = 0; i < numbers.Length; i++)
-   {
-    if (i == numbers.Length-1)
     {
-        Console.Write($"{numbers[i]}.");
+        if (i == numbers.Length - 1)
+        {
+            Console.Write($"{numbers[i]}.");
+        }
+        else
+        {
+            Console.Write($"{numbers[i]}, ");
+        }
     }
-    else
-    {
-        Console.Write($"{numbers[i]}, ");
-    }
-   }
 }
 
 MathNumbers();
