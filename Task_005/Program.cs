038df7c4a09984ff919b7d2c56b6e5c26aa68a5a
ritesu <ritesu@mail.ru>
2023-02-14 01:41:01 +1100

    Task_005

---
+++ Task_005/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine()??"");
+int[] numbers = new int[0];
+
+void CorrectNumbers()
+{
+    int count = 0;
+    for (int a = numb; a > 0;)
+    {
+        a = a/10;
+        count++;
+    }
+    numbers = new int [count];
+}
+
+void FillNumbers()
+{
+    int a = numb;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = a%10;
+        a = a/10;
+    }
+}
+
+int sum = 0;
+void SumNumbers()
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum = numbers[i]+sum;
+    }
+}
+
+CorrectNumbers();
+FillNumbers();
+SumNumbers();
+Console.WriteLine($"Сумма цифр числа {numb} равна {sum}");
