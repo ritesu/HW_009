a26f3c1bb25141adf445cac5c889d1b7d7c5df2b
ritesu <ritesu@mail.ru>
2023-02-14 00:01:47 +1100

    Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Укажите последнее значение числового ряда");
+int a = int.Parse(Console.ReadLine() ?? "");
+int[] numbers = Enumerable.Range(1, a).ToArray();
+void PrintNumbers(int[] numbers)
+{
+    foreach (int i in numbers)
+    {
+        if (i == a)
+        {
+            Console.Write($"{i}.");
+        }
+        else
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+PrintNumbers(numbers);
