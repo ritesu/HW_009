070c43ad333cacce8fc2851a60e780a9836f3b90
ritesu <ritesu@mail.ru>
2023-02-14 02:08:55 +1100

    Task_006

---
+++ Task_006/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите два значения для вычисления");
+int n = int.Parse(Console.ReadLine() ?? "");
+int m = int.Parse(Console.ReadLine() ?? "");
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (n > 0 && m == 0)
+    {
+        return A(n - 1, 1);
+    }
+    if (n > 0 && m > 0)
+    {
+        return A(n - 1, A(n, m - 1));
+    }
+    return A(n, m);
+}
+A(n, m);
+Console.WriteLine($"Значение функции Аккермана равна {A(n, m)}");
