14b30ec78b0a20a1c4f4a39d9764872f1e3cc750
ritesu <ritesu@mail.ru>
2023-02-14 00:06:23 +1100

    Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите с какого значения показать числовой ряд");
+int a = int.Parse(Console.ReadLine()??"");
+int[] numbers = Enumerable.Range(1, a).ToArray();
+Array.Reverse(numbers);
+void PrintNumbers(int[] numbers)
+{
+    foreach (int i in numbers)
+    {
+        if (i == 1)
+        {
+            Console.Write($"{i}.");
+        }
+        else
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+PrintNumbers(numbers);
