d08da05dcb68d6e5667b15a5e2ac8e575c32a098
ritesu <ritesu@mail.ru>
2023-02-14 02:22:54 +1100

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число, которе нужно возвести в степень");
+int a = int.Parse(Console.ReadLine()??"");
+Console.WriteLine("Введите степень");
+int b = int.Parse(Console.ReadLine()??"");
+int result = a;
+
+void Result()
+{
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+}
+Result();
+Console.WriteLine($"Число {a} возведенное в степень {b} равно {result}");
